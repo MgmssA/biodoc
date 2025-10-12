847f8539bca59f4154be8fa13786d25283ff786e
MgmssA <223298454+MgmssA@users.noreply.github.com>
2025-10-12 15:06:40 +0200

    All asset dirs outside extension dir

--- .includes/katex_macros.js
+++ .includes/katex_macros.js
@@ -3,5 +3,5 @@ for (let element of mathElements) {
     katex.render(element.textContent, element, {
         throwOnError: false,
         macros
-    };
+    });
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# <%= title %> Format
+# Biodoc Format
 
 ## Installing
 
